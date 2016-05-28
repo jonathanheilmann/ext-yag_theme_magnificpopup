ec738e62c22501303ccf96b354c114b614bee9cb
jonathanheilmann <mail@jonathan-heilmann.de>
2016-05-28 15:41:39 +0200

    [TASK] Set version to 0.2.0-dev, update dependencies and clean up

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,39 +1,42 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "yag_theme_magnificpopup".
-#
-# Auto generated 07-08-2015 22:18
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "yag_theme_magnificpopup".
+ *
+ * Auto generated 20-11-2013 21:25
+ *
+ * Manual updates:
+ * Only the data in the array - anything else is removed by next write.
+ * "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'YAG Theme Magnific Popup',
 	'description' => 'Theme for YAG gallery that integrates Magnific Popup for displaying images.',
 	'category' => 'plugin',
-	'shy' => 0,
-	'version' => '0.1.4',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'TYPO3_version' => '4.5.0-6.2.99',
-	'PHP_version' => '',
-	'module' => '',
-	'state' => 'beta',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
 	'author' => 'Jonathan Heilmann',
 	'author_email' => 'mail@jonathan-heilmann.de',
-	'author_company' => 'Webprogrammierung Jonathan Heilmann',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
+	'author_company' => '',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => '0',
+	'lockType' => '',
+	'version' => '0.2.0-dev',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.6.99',
+			'yag' => '4.0.0',
+		),
+		'conflicts' => array(
+			'yag' => '0.0.0-3.2.99',
+		),
+		'suggests' => array(
+		),
+	),
 );
-
-?>
